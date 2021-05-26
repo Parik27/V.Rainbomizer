1b64757b0b9229a78e346c96ff388024d24a8ab5
Parik <pariks@protonmail.com>
2021-05-27 04:29:42 +0530

    General: Add remaining data files and descriptions

---
+++ data/update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget -q -O "VehiclePatterns.txt" "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ2rABM-2URF36pbTX9XjYp2mk5frjxK5YQ62Gk6tVMXzfFVBDxKnmI5vnFNYzSJgqzB8siyz3Sw3bI/pub?gid=0&single=true&output=tsv"
+wget -q -O "Missions.txt" "https://docs.google.com/spreadsheets/d/e/2PACX-1vTAaX8GjoI0yyWsQO5KtaCxw5SZTDfGQsPbky3c4BGRKmIaaqH9pKR1ZflVvMPWnCNz1cmw7i9gtpr1/pub?gid=0&single=true&output=tsv"
+wget -q -O "WeaponWeights.txt" "https://docs.google.com/spreadsheets/d/e/2PACX-1vS48aixOSWbSrfd70Ufd8EJzEWWVxpdUvdl3sonmphmfprha-yEXdlzlDTGLvLGjoNf7ZXPafY79jFn/pub?gid=884612377&single=true&output=tsv"
